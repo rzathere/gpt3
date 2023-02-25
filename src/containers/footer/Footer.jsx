1d23afc83c14717cb6cb6a7f3217fcca40a760ed
RZA <77061473+rzathere@users.noreply.github.com>
2023-02-25 11:54:13 +0530

    Add files via upload

---
+++ src/containers/footer/Footer.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import gpt3Logo from '../../assets/logo.svg';
+import './footer.css';
+
+const Footer = () => (
+  <div className="gpt3__footer section__padding">
+    <div className="gpt3__footer-heading">
+      <h1 className="gradient__text">Do you want to step in to the future before others</h1>
+    </div>
+
+    <div className="gpt3__footer-btn">
+      <p>Request Early Access</p>
+    </div>
+
+    <div className="gpt3__footer-links">
+      <div className="gpt3__footer-links_logo">
+        <img src={gpt3Logo} alt="gpt3_logo" />
+        <p> <br /> All Rights Reserved <br />by <br /> Team Educators</p>
+      </div>
+      <div className="gpt3__footer-links_div">
+        <h4>Links</h4>
+        <p>Overons</p>
+        <p>Social Media</p>
+        <p>Counters</p>
+        <p>Contact</p>
+      </div>
+      <div className="gpt3__footer-links_div">
+        <h4>Company</h4>
+        <p>Terms & Conditions </p>
+        <p>Privacy Policy</p>
+        <p>Contact</p>
+      </div>
+      <div className="gpt3__footer-links_div">
+        <h4>Get in touch</h4>
+        <p>Team Educators</p>
+        <p>+91 8249427802</p>
+        <p>info@the-educators.com</p>
+      </div>
+    </div>
+
+    <div className="gpt3__footer-copyright">
+      <p>@2023 Team Educators. All rights reserved.</p>
+    </div>
+  </div>
+);
+
+export default Footer;
